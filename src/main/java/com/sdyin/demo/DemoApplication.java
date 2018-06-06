fea7f9f3f550431b7cf7e4de61f9c7b5096078bb
liuye <liuye@iboxpay.com>
2018-06-06 13:41:58 +0800

    add

--- src/main/java/com/sdyin/demo/DemoApplication.java
+++ src/main/java/com/sdyin/demo/DemoApplication.java
@@ -2,6 +2,7 @@ package com.sdyin.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +11,10 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@RequestMapping(value="test")
+	public String test(){
+		return "test";
 	}
 }
