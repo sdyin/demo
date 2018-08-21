870b647e4ac6a72bd087dcebc16d10ca3eb6b591
liuye <liuye@iboxpay.com>
2018-08-21 17:43:47 +0800

    测试流

--- src/main/java/com/sdyin/demo/java8/stream/Test1.java
+++ src/main/java/com/sdyin/demo/java8/stream/Test1.java
@@ -17,16 +17,20 @@ public class Test1 {
     Student lisa = new Student(1, "lisa", 21);
     Student mike = new Student(1, "mike", 22);
     Student sdyin = new Student(1, "sdyin", 23);
+    Student lilei = new Student(1, "lilei", 24);
+    Student hanmeimei = new Student(1, "hanmeimei", 24);
     List<Student> stus = new ArrayList<>();
     stus.add(lisa);
     stus.add(mike);
     stus.add(sdyin);
+    stus.add(lilei);
+    stus.add(hanmeimei);
 
 
     stus.stream()
             .filter(stu-> {
               System.out.println("filter:"+stu.getName());
-              return stu.getAge()>21;
+              return stu.getAge()>20;
             })
             .map(stu->{
               System.out.println("map:"+ stu.getName());
@@ -35,7 +39,7 @@ public class Test1 {
             .limit(3)
             .collect(toList());
 
-    System.out.println("-----------------");
+    System.out.println("--------------------------");
 
     stus.parallelStream()
             .filter(stu-> {
