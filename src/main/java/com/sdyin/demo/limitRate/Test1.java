e2def4465ad61818a8b6e36939861419b3dc970b
liuye <liuye@iboxpay.com>
2018-08-27 23:20:55 +0800

    测试限流测试

--- src/main/java/com/sdyin/demo/limitRate/Test1.java
+++ src/main/java/com/sdyin/demo/limitRate/Test1.java
@@ -2,6 +2,8 @@ package com.sdyin.demo.limitRate;
 
 import com.google.common.util.concurrent.RateLimiter;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * 限流算法:令牌算法
  * @author: liuye
@@ -13,11 +15,12 @@ public class Test1 {
   public static void main(String[] args) {
 
     //令牌桶每秒钟产生两个令牌
+    //RateLimiter.create(2,3, TimeUnit.SECONDS);
     RateLimiter rateLimiter = RateLimiter.create(2);
     System.out.println("是否可执行:"+rateLimiter.tryAcquire());
     System.out.println(rateLimiter.acquire(5));
     System.out.println("是否可执行:"+rateLimiter.tryAcquire());
-    System.out.println(rateLimiter.acquire(2));
+    System.out.println(rateLimiter.acquire(7));
     System.out.println("是否可执行:"+rateLimiter.tryAcquire());
     System.out.println(rateLimiter.acquire(1));
 
