8cf3a76da0567f5dd15bd20a74ddbeba27e058ad
liuye <liuye@iboxpay.com>
2018-10-19 20:50:41 +0800

    增加 git push demo2

--- src/main/java/com/sdyin/demo/git/demo/Demo1.java
+++ src/main/java/com/sdyin/demo/git/demo/Demo1.java
@@ -8,5 +8,6 @@ package com.sdyin.demo.git.demo;
 public class Demo1 {
   public static void main(String[] args) {
     System.out.println("git push demo1");
+    System.out.println("git push demo2");
   }
 }
